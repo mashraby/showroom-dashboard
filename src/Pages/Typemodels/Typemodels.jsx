2ed34055d52254ab0524a9121549cb4f302b0a63
Asrorbek Shahobiddinov <asror797shahobiddinov@gmail.com>
2023-01-27 16:38:51 +0500

    mfenf

--- src/Pages/Typemodels/Typemodels.jsx
+++ src/Pages/Typemodels/Typemodels.jsx
@@ -3,11 +3,24 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../../Components/Navbar/Navbar";
+import accounting from "accounting";
 
 export default function Typemodels() {
   const { id } = useParams();
 
   // comment
+
+  const ModelCost = (confs) => {
+    let modelCost = 0 ;
+
+    // if()
+    confs?.map(e => {
+      return modelCost = modelCost + (e.cost*e.running_qty);
+    })
+
+    return modelCost;
+
+  }
 
   const [model , setModel ] = useState()
 
@@ -16,6 +29,7 @@ export default function Typemodels() {
       .get(`/model/${id}`)
       .then((res) => {
          console.log(res.data)
+    
          setModel(res.data)
       })
       .catch((err) => console.log(err));
@@ -32,18 +46,67 @@ export default function Typemodels() {
           <p>price 3: <b>{model?.price3}</b> %</p> */}
         <div className="edit-price-section">
           <div>
+            <h3>Factory</h3>
+            <p>Cost: <b>{accounting.formatNumber(ModelCost(model?.configurations ? model.configurations : []),0," ")}</b> So'm</p>
+            <p>Percent: {model.price1} %</p>
+            <p>Avarage: {5992000}</p>
+          </div>
+          <div>
+            <h3>Showroom</h3>
+            <p>Cost: <b>{588246000}</b></p>
+            <p>percent: {model.price2} %</p>
+            <p>Avarage: 547893000</p>
+          </div>
+          <div>
+            <h3>Lorem, ipsum.</h3>
             <p>Lorem, ipsum dolor.</p>
           </div>
           <div>
-            <p>Lorem, ipsum dolor.</p>
-          </div>
-          <div>
-            <p>Lorem, ipsum dolor.</p>
-          </div>
-          <div>
-            <p>Lorem, ipsum dolor.</p>
+            <h3>Aksiya</h3>
+            <p>Cost: <b>458752000</b></p>
           </div>
         </div>
+
+        <h1 style={{color:'white',textAlign:'center',padding:25}}>Configuration</h1>
+        <div className='shelf'>
+         <table>
+            <thead>
+               <tr>
+                  <th>ID</th>
+                  <th>Cost</th>
+                  <th>Quantity</th>
+                  <th>Title</th>
+                  <th>Status</th>
+                  <th>Settings</th>
+               </tr>
+            </thead>
+            <tbody>
+               {model?.configurations?.map((model,index) => {
+                  return(
+                     <tr key={index}>
+                        <td>{index+1}</td>
+                        <td className='titleBook'>{model.cost ? accounting.formatNumber(model.cost,0," ") : "undefined"} so'm</td>
+                        <td> <button>-</button> {model.running_qty} <button>+</button></td>
+                        <td>{model.title ? model.title : "undefined"}</td>
+                        <td>active</td>
+                        <td>
+                           <button 
+                              data-id = {model.id}
+                              // onClick={editBtnHandler}
+                              className='editbtn'>Edit</button>
+                           <button 
+                              data-isbn = {model.id}
+                              // onClick={deleteBtnHandler}
+                              className='delbtn'>
+                              Delete
+                           </button>
+                        </td>
+                     </tr>
+                  )
+               })}
+            </tbody>
+         </table>
+         </div>
       </div>
     </div>
   );
